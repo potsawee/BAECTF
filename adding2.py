63e1bc189bbc6b9f9816ef112e5797e4bff8e4ef
Potsawee Manakul <m.potsawee@gmail.com>
2016-11-12 13:30:15 +0000

    try finding the key for adding

--- adding2.py
+++ adding2.py
@@ -1,6 +1,7 @@
 import getpass
 import sys
 import telnetlib
+import re
 
 HOST = 'skillz.baectf.com'
 #password = getpass.getpass()
@@ -12,8 +13,8 @@ tn.read_until("Enter password: ")
 tn.write("avenue\n")
 a = tn.read_until("Here are the numbers: ",2)
 
+#get 4 numbers and find the sum
 
-
-#print "HIIIIIi"
-print a
-#print "PUNPUN"
+tn.write(sum+"\n")
+key = tn.read_until(".",2)
+print key
